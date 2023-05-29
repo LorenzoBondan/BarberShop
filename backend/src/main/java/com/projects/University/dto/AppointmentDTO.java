ba7104d43929b76d62a8fd533c78d9cebea5e1f8
LorenzoBondan <lbbondan@gmail.com>
2023-05-29 14:19:52 -0300

    appointment dto

---
+++ backend/src/main/java/com/projects/University/dto/AppointmentDTO.java
@@ -0,0 +1,93 @@
+package com.projects.University.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.validation.constraints.NotBlank;
+
+import com.projects.University.entities.Appointment;
+import com.projects.University.entities.User;
+
+public class AppointmentDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	@NotBlank(message = "Campo obrigatório")
+	private LocalDateTime dateTime;
+	@NotBlank(message = "Campo obrigatório")
+	private User barber;
+	@NotBlank(message = "Campo obrigatório")
+	private User client;
+	
+	public AppointmentDTO() {}
+	
+	public AppointmentDTO(Long id, LocalDateTime dateTime, User barber, User client) {
+		super();
+		this.id = id;
+		this.dateTime = dateTime;
+		this.barber = barber;
+		this.client = client;
+	}
+
+	public AppointmentDTO(Appointment entity) {
+		super();
+		this.id = entity.getId();
+		this.dateTime = entity.getDateTime();
+		this.barber = entity.getBarber();
+		this.client = entity.getClient();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public User getBarber() {
+		return barber;
+	}
+
+	public void setBarber(User barber) {
+		this.barber = barber;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AppointmentDTO other = (AppointmentDTO) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
