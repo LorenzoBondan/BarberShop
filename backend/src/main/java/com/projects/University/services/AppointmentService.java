b30b9b1710ccc655c1211ddc47a2386fbfd0de8a
LorenzoBondan <lbbondan@gmail.com>
2023-05-29 14:29:09 -0300

    appointment service

---
+++ backend/src/main/java/com/projects/University/services/AppointmentService.java
@@ -0,0 +1,62 @@
+package com.projects.University.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.projects.University.dto.AppointmentDTO;
+import com.projects.University.entities.Appointment;
+import com.projects.University.repositories.AppointmentRepository;
+import com.projects.University.services.exceptions.DataBaseException;
+import com.projects.University.services.exceptions.ResourceNotFoundException;
+
+public class AppointmentService {
+	
+	@Autowired
+	private AppointmentRepository repository;
+	
+	@Transactional(readOnly = true)
+	public Page<AppointmentDTO> findAllPaged(Pageable pageable) {
+		Page<Appointment> list = repository.findAll(pageable);
+		return list.map(x -> new AppointmentDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public AppointmentDTO findById(Long id) {
+		Optional<Appointment> obj = repository.findById(id);
+		Appointment entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found."));
+		return new AppointmentDTO(entity);
+	}
+	
+	@Transactional
+	public AppointmentDTO insert(AppointmentDTO dto) {
+		Appointment entity = new Appointment();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new AppointmentDTO(entity);
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+
+		catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity Violation");
+		}
+	}
+	
+	private void copyDtoToEntity(AppointmentDTO dto, Appointment entity) {
+		entity.setDateTime(dto.getDateTime());
+		entity.setBarber(dto.getBarber());
+		entity.setClient(dto.getClient());
+	}
+
+}
