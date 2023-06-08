8e28d5fddd862e066659acb4c8eef252980f26b2
LorenzoBondan <lbbondan@gmail.com>
2023-06-08 11:44:41 -0300

    about us component

---
+++ frontend/src/components/AboutUs/index.tsx
@@ -0,0 +1,20 @@
+
+import './styles.css';
+
+const AboutUs = () => {
+    return(
+        <div className='about-us-container'>
+            <div className='about-us-content-container'>
+                <h4>About Us</h4>
+                <span className='separator'></span>
+                <p>More than just a barbershop, here you are at home.<br />Don Manera Barbiere offers the best products, quality services, and exceptional customer care in a comfortable and cozy environment, so you can feel completely at ease. We want you to feel like you're among friends here, not just coming to get a haircut or a shave. Stop by, have a chat, listen to some music, enjoy a cold beer. Bring along other friends and take advantage of this unique experience that only we can provide.</p>
+            </div>
+            <div className='about-us-image-container'>
+                <img src="https://donmanera.com.br/site/wp-content/uploads/2021/08/barbearia.jpg" alt="" />
+            </div>
+            
+        </div>
+    );
+}
+
+export default AboutUs;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -3,12 +3,16 @@ import Banner from 'components/Banner';
 import './styles.css';
 import Barbers from 'components/Barbers';
 import Location from 'components/Location';
+import AboutUs from 'components/AboutUs';
 
 const Home = () => {
     return(
         <div className='home-container'>
             <div className='home-container-banner'>
                 <Banner/>
+            </div>
+            <div className='home-container-about-us'>
+                <AboutUs/>
             </div>
             <div className='home-container-barbers'>
                 <Barbers/>
